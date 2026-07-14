e6ff32de51ebaccc4ecab066d44027776dfc8ed5
Cody G. <cgunter13@ivytech.edu>
2026-07-13 22:11:04 -0400

    Continuation of code-a-long, following Mr. Hamby's video.

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
+//For the sake of following along with the video, I've commented out what Mr. Hamby has deleted from his.
+
+
 const express = require("express")
 const app = express()
+const router = express.Router()
 
-app.listen(3000,function(){
-    console.log("Listening on port 3000")
+
+//app.listen(3000,function(){
+    //console.log("Listening on port 4000")
+//})
+
+router.get("/songs", function(req,res){
+    const songs = [
+        {
+            title: "Home Is For The Heartless",
+            artist: "Parkway Drive",
+            popularity: 10,
+            genre: ["metal", "metalcore"],
+        },
+        {
+            title: "Dehumanized",
+            artist: "Bring Me The Horizon",
+            popularity: 10,
+            genre: ["metal", "deathcore"],
+        }    
+    ]
+
+    res.json(song)
 })
+
+app.use("/api", router)
+app.listen(3000)
 
 //app.get("/hello", function(req,res){
     //res.send("<h1>Hello Express</h1>")
