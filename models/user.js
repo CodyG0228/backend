beafd7a9f5d772e2de05c9b877e436d06c4b13c7
CodyG0228 <codyrg0228@gmail.com>
2026-07-28 12:30:54 -0400

    starting auth work

--- models/user.js
+++ models/user.js
@@ -1,9 +1,8 @@
 const db = require("../db");
 
-// Create a model from the schema
-const User = db.model("User",{
-    username: {type: String, required: true},
-    password: {type: String, required: true},
+const User = db.model("User", {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
     status: String
 })
 
