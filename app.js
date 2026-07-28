90fc4301dd354ff566d27058a126adad92ffa492
CodyG0228 <codyrg0228@gmail.com>
2026-07-28 12:57:05 -0400

    beefing up auth. protected routes with authentication

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //For the sake of following along with the video, I've commented out what Mr. Hamby has deleted from his.
 
-
+const User = require("./models/user")
+const jwt = require("jwt-simple")
 const express = require("express")
 const Song = require("./models/songs")
 var cors = require('cors')
@@ -11,16 +12,18 @@ app.use(cors())
 app.use(express.json());
 
 const router = express.Router()
-
-router.get("/songs", async(req,res) =>{
-   try{
-      const songs = await Song.find({})
-      res.send(songs)
-      console.log(songs)
-   }
-   catch (err){
-      console.log(err)
-   }
+const secret = "mysecretkey"
+router.get("/songs", authMiddleware, async(req,res) =>{
+   router.post("/songs", authMiddleware, async(req,res) =>{
+      try{
+         const songs = await Song.find({})
+         res.send(songs)
+         console.log(songs)
+      }
+      catch (err){
+         console.log(err)
+      }
+   })
 
 })
 
@@ -38,6 +41,47 @@ router.post("/songs", async(req,res) =>{
       
    
 })
+
+router.post("/register", async (req, res) => {
+    try {
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        if (!user) {
+            return res.status(401).send("USER NOT FOUND. Have you registered?")
+        }
+        if (user.password !== req.body.password) {
+            return res.status(401).send("Incorrect password. Double check spelling.")
+        }
+        const token = jwt.encode({ username: user.username }, secret)
+        res.json({ token: token })
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+function authMiddleware(req, res, next) {
+    const token = req.headers["x-auth"] 
+    if (!token) {
+        return res.status(401).send("---NO TOKEN PROVIDED---")
+    }
+    try {
+        const decoded = jwt.decode(token, secret)
+        req.user = decoded
+        next()
+    } catch (err) {
+        res.status(401).send("---INVALID TOKEN---")
+    }
+}
+
 
 //const bodyParser = require("body-parser")
 
