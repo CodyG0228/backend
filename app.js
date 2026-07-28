efb417d778749fa2410c85caaa4d563336734e3f
CodyG0228 <codyrg0228@gmail.com>
2026-07-28 11:44:05 -0400

    updated app.js for main

--- app.js
+++ app.js
@@ -1,80 +1,20 @@
-//For the sake of following along with the video, I've commented out what Mr. Hamby has deleted from his.
-
-
+//setup.. this is similar to when we use our default tags in html
 const express = require("express")
-const Song = require("./models/songs")
-var cors = require('cors')
-
+//activate or tell this app variable to be an express server
 const app = express()
-app.use(cors())
 
-app.use(express.json());
-app.use(bodyParser.json());
-const router = express.Router()
-
-router.get("/songs", async(req,res) =>{
-   try{
-      const songs = await Song.find({})
-      res.send(songs)
-      console.log(songs)
-   }
-   catch (err){
-      console.log(err)
-   }
-
+//start the web server... app.listen(portnumber,function)
+app.listen(3000,function(){
+    console.log("Listening on port 3000")
 })
 
-router.post("/songs", async(req,res) =>{
-   try{
-      const song = await new Song(req.body)
-      await song.save()
-      res.status(201).json(song)
-      console.log(song)
-   }
-   catch(err){
-      res.status(400).send(err)
+//making an api using routes
+// Routes are used to handle browser requests.  The look like URLs.  The difference is that when a browser requests a route, it is dynamically handled by using a function.
 
-   }
-      
-   
+//GET or a regular request when someone goes to http://localhost:3000/hello.  When using a function an a route, we almost always have a parameter or handle a response and request
+app.get("/hello", function(req,res){
+    res.send("<h1>Hello Express</h1>")
 })
-
-//const bodyParser = require("body-parser")
-
-
-
-
-
-//app.listen(3000,function(){
-    //console.log("Listening on port 4000")
-//})
-
-// router.get("/songs", function(req,res){
-//     const songs = [
-//         {
-//             title: "Home Is For The Heartless",
-//             artist: "Parkway Drive",
-//             popularity: 10,
-//             genre: ["metal", "metalcore"],
-//         },
-//         {
-//             title: "Dehumanized",
-//             artist: "Bring Me The Horizon",
-//             popularity: 10,
-//             genre: ["metal", "deathcore"],
-//         }    
-//     ]
-
-//     res.json(song)
-// })
-
-app.use("/api", router)
-app.listen(3000)
-
-//app.get("/hello", function(req,res){
-    //res.send("<h1>Hello Express</h1>")
-//})
-
-//app.get("/goodbye", function(req, res){
-    //res.send("<h1>Goodbye, Express</h1>")
-//});
+app.get("/goodbye", function(req, res) {
+    res.send("<h1>Goodbye, Express!</h1>");
+ });
