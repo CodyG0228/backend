740f36a5a88cb8daf08dfc32f2177d42f267267b
CodyG0228 <codyrg0228@gmail.com>
2026-07-27 22:13:44 -0400

    still following guide

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-//For the sake of following along with the video, I've commented out what Mr. Hamby has deleted from his.
+// //For the sake of following along with the video, I've commented out what Mr. Hamby has deleted from his.
 
 
-const express = require("express")
-const Song = require("./models/songs")
+const express = require("express");
+const Song = require("./models/song");
 var cors = require('cors')
 
-const app = express()
+const app = express();
 app.use(cors())
 
+// Middleware that parses HTTP requests with JSON body
 app.use(express.json());
-app.use(bodyParser.json());
-const router = express.Router()
 
+const router = express.Router();
+
+// Get list of all songs in the database
 router.get("/songs", async(req,res) =>{
    try{
       const songs = await Song.find({})
@@ -39,42 +41,88 @@ router.post("/songs", async(req,res) =>{
    
 })
 
-//const bodyParser = require("body-parser")
+
+app.use("/api", router);
+
+app.listen(3000);
 
 
 
 
+// const express = require("express")
+// const Song = require("./models/songs")
+// var cors = require('cors')
 
-//app.listen(3000,function(){
-    //console.log("Listening on port 4000")
-//})
+// const app = express()
+// app.use(cors())
 
-// router.get("/songs", function(req,res){
-//     const songs = [
-//         {
-//             title: "Home Is For The Heartless",
-//             artist: "Parkway Drive",
-//             popularity: 10,
-//             genre: ["metal", "metalcore"],
-//         },
-//         {
-//             title: "Dehumanized",
-//             artist: "Bring Me The Horizon",
-//             popularity: 10,
-//             genre: ["metal", "deathcore"],
-//         }    
-//     ]
+// app.use(express.json());
+// // app.use(bodyParser.json());
+// const router = express.Router()
 
-//     res.json(song)
+// router.get("/songs", async(req,res) =>{
+//    try{
+//       const songs = await Song.find({})
+//       res.send(songs)
+//       console.log(songs)
+//    }
+//    catch (err){
+//       console.log(err)
+//    }
+
 // })
 
-app.use("/api", router)
-app.listen(3000)
+// router.post("/songs", async(req,res) =>{
+//    try{
+//       const song = await new Song(req.body)
+//       await song.save()
+//       res.status(201).json(song)
+//       console.log(song)
+//    }
+//    catch(err){
+//       res.status(400).send(err)
 
-//app.get("/hello", function(req,res){
-    //res.send("<h1>Hello Express</h1>")
-//})
+//    }
+      
+   
+// })
 
-//app.get("/goodbye", function(req, res){
-    //res.send("<h1>Goodbye, Express</h1>")
-//});
+// //const bodyParser = require("body-parser")
+
+
+
+
+
+// //app.listen(3000,function(){
+//     //console.log("Listening on port 4000")
+// //})
+
+// // router.get("/songs", function(req,res){
+// //     const songs = [
+// //         {
+// //             title: "Home Is For The Heartless",
+// //             artist: "Parkway Drive",
+// //             popularity: 10,
+// //             genre: ["metal", "metalcore"],
+// //         },
+// //         {
+// //             title: "Dehumanized",
+// //             artist: "Bring Me The Horizon",
+// //             popularity: 10,
+// //             genre: ["metal", "deathcore"],
+// //         }    
+// //     ]
+
+// //     res.json(song)
+// // })
+
+// app.use("/api", router)
+// app.listen(3000)
+
+// //app.get("/hello", function(req,res){
+//     //res.send("<h1>Hello Express</h1>")
+// //})
+
+// //app.get("/goodbye", function(req, res){
+//     //res.send("<h1>Goodbye, Express</h1>")
+// //});
