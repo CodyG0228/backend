abe29bc18156cf12aab1fad0708647b6ba439bc9
CodyG0228 <codyrg0228@gmail.com>
2026-07-28 12:08:29 -0400

    problem with MongoDB connection string

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://codyrg0228_db_user:Co1yaZZMULVawv5h@songdb.iyeookj.mongodb.net/?appName=SongDB",{useNewURLParser: true})
-
+mongoose.connect("mongodb+srv://codyrg0228_db_user:Co1yaZZMULVawv5h@songdb.iyeookj.mongodb.net/?appName=SongDB")
 module.exports = mongoose
